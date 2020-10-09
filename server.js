8dc2ac51f724c1f39d9dea21771f7b76059e90d4
The Dictator <52343099+pramod54@users.noreply.github.com>
2020-10-09 16:12:00 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const socketio = require('socket.io');
+const formatMessage = require('./public/utils/messages');
+const {userJoin,getCurrentUser,userLeave, getRoomUsers} = require('./public/utils/users');
+
+const app = express();
+const server = http.createServer(app);
+const io =socketio(server);
+///Set static folder
+app.use(express.static(path.join(__dirname,'public')))
+var botName = 'ChatBot';
+//run when client connects
+io.on('connection', socket =>{
+    socket.on('joinRoom',({username,room}) => {
+        const user = userJoin(socket.id,username,room);
+
+        socket.join(user.room);
+
+
+
+        //Welcome current user
+    socket.emit('message',formatMessage('botName','Welcome to ChatBox!'));
+
+    //Broadcast when a user connects
+    socket.broadcast.to(user.room).emit('message', formatMessage(botName,`${user.username}has joined the chat`)); 
+
+    //send user and room info
+    io.to(user.room).emit('roomUsers',{
+        room: user.room,
+        users: getRoomUsers(user.room)
+    })
+
+    })
+    
+   
+    //listen for chatMessage
+    socket.on('chatMessage', (msg) => {
+        const user = getCurrentUser(socket.id);
+       io.to(user.room).emit('message',formatMessage(user.username,msg));
+
+    });
+     //Run when client disconnects
+     socket.on('disconnect', ()  => {
+         const user = userLeave(socket.id);
+
+         if(user){
+            io.to(user.room).emit('message', formatMessage(botName,`${user.username} has left the chat`));
+
+            io.to(user.room).emit('roomUsers',{
+                room: user.room,
+                users: getRoomUsers(user.room)
+            })
+         }
+    })
+
+})
+const PORT = 3000 || process.nextTick.PORT;
+
+server.listen(PORT,() => console.log(`Server running on  port ${PORT}`));
